f448b69ada845e2f7756c76184101e1a53a5cb47
aalteirac <anthony@alteirac.com>
2022-10-11 09:29:51 +0200

    check val

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ def main():
     st.write("## Example")
     value = sr.streamlit_searchbar(label="Search Customer...")
     st.write(value)
-    st.button("test")
+    if st.button("Check"):
+        st.info(value + "has been typed")
+
 
 
 if __name__ == "__main__":
